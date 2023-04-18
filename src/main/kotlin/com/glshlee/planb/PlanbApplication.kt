239b649f42f1de9df6c32192194991487038a451
glshlee <glshlee@gmail.com>
2023-04-19 00:48:00 +0900

    init project

---
+++ src/main/kotlin/com/glshlee/planb/PlanbApplication.kt
@@ -0,0 +1,11 @@
+package com.glshlee.planb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlanbApplication
+
+fun main(args: Array<String>) {
+	runApplication<PlanbApplication>(*args)
+}
