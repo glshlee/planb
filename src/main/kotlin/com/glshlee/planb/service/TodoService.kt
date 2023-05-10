ede7935f2f85241047ec47a66374434d9060440b
glshlee <glshlee@gmail.com>
2023-05-11 00:04:00 +0900

    refactor: added validate method for todo entity

--- src/main/kotlin/com/glshlee/planb/service/TodoService.kt
+++ src/main/kotlin/com/glshlee/planb/service/TodoService.kt
@@ -14,6 +14,15 @@ class TodoService(
 
     fun create(entity: Todo): List<Todo> {
         // validations
+        validate(entity)
+
+        todoRepository.save(entity)
+        logger.info("Entity Id ${entity.id} is saved.")
+
+        return todoRepository.findByUserId(entity.userId)
+    }
+
+    private fun validate(entity: Todo) {
         if (entity == null) {
             logger.warn("Entity cannot be null.")
             throw RuntimeException("Entity cannot be null.")
@@ -23,10 +32,5 @@ class TodoService(
             logger.warn("Unknown user.")
             throw RuntimeException("Unknown user.")
         }
-
-        todoRepository.save(entity)
-        logger.info("Entity Id ${entity.id} is saved.")
-
-        return todoRepository.findByUserId(entity.userId)
     }
 }
