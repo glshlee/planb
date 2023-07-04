cc8daac76766371cdadb1f60e25812fa56febec1
glshlee <glshlee@gmail.com>
2023-07-05 00:19:58 +0900

    added enterKeyEventHandler

--- frontend/todo-react-app/src/AddTodo.js
+++ frontend/todo-react-app/src/AddTodo.js
@@ -15,10 +15,16 @@ const AddTodo = (props) => {
         console.log(item);
     };
 
+    const enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter') {
+            onButtonClick();
+        }
+    }
+
     return (
         <Grid container style={{ marginTop: 20 }}>
             <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-                <TextField placeholder="Add Todo here" fullWidth onChange={onInputChange} value={item.title} />
+                <TextField placeholder="Add Todo here" fullWidth onChange={onInputChange} onKeyDown={enterKeyEventHandler} value={item.title} />
             </Grid>
             <Grid xs={1} md={1} item>
                 <Button fullWidth style={{ height: '100%' }} color="secondary" variant="outlined" onClick={onButtonClick}></Button>
